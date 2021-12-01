5f460aaa56a3c048e2d4c45c72b0db0875fb26ea
Paulo Coutinho <paulo@ubook.com>
2021-11-30 22:11:55 -0300

    new version

--- tests/test_pygems.py
+++ tests/test_pygems.py
@@ -3,4 +3,4 @@ from pygemstones import __version__
 
 # -----------------------------------------------------------------------------
 def test_version():
-    assert __version__ == "0.0.4"
+    assert __version__ == "0.0.5"
