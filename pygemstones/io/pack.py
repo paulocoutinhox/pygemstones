0790e6e7ac679fc3dd366f187bba3d547a62edd8
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-24 13:33:46 +0000

    Adding tarfile member sanitization to extractall()

--- pygemstones/io/pack.py
+++ pygemstones/io/pack.py
@@ -37,7 +37,26 @@ def unpack(src_path, dst_path):
         ".gz",
     ]:
         with tarfile.open(src_path, "r:*") as archive:
-            archive.extractall(dst_path)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(archive, dst_path)
             archive.close()
     else:
         raise Exception("File format not supported: {0}".format(file_extension))
